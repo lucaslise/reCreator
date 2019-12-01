8666e16edcfed52cc26cc61956f768c35cf2bdb5
Lucas Lise <lucas_lise1@hotmail.com>
2019-12-01 14:28:16 -0300

    Updates

---
+++ precache-manifest.94987f5cacf2686bd5f785dfe963a10b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f977246e986973f1293057856b066461",
+    "url": "/reCreator/index.html"
+  },
+  {
+    "revision": "3a063a95e00a7d43bf27",
+    "url": "/reCreator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "64303c622583d0ac1175",
+    "url": "/reCreator/static/js/2.1c47e9a6.chunk.js"
+  },
+  {
+    "revision": "3a063a95e00a7d43bf27",
+    "url": "/reCreator/static/js/main.3dd768fd.chunk.js"
+  },
+  {
+    "revision": "6b22d82d04518473d2bf",
+    "url": "/reCreator/static/js/runtime~main.7c91e958.js"
+  }
+]);
